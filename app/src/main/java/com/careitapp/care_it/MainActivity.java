25cf9d87e29a56139b5bcece47075f05205b454f
Sandy <sxie@gatech.edu>
2018-10-20 16:45:51 -0400

    fixed contacts

--- app/src/main/java/com/careitapp/care_it/MainActivity.java
+++ app/src/main/java/com/careitapp/care_it/MainActivity.java
@@ -1,13 +1,8 @@
 package com.careitapp.care_it;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
