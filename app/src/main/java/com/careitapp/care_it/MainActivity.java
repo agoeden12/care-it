f0cd168f883d08eed1d6b3999f99b5e7dff66aa4
smwhitt <smw81@duke.edu>
2018-10-20 01:20:56 -0400

    vision

--- app/src/main/java/com/careitapp/care_it/MainActivity.java
+++ app/src/main/java/com/careitapp/care_it/MainActivity.java
@@ -1,7 +1,10 @@
 package com.careitapp.care_it;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, Vision.class)));
     }
 }
