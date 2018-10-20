8db19a8b761a1e9013c728221608f521f5723a79
Andrew Goeden <andrewgoeden@gmail.com>
2018-10-20 19:42:35 -0400

    ocr hopefully works

---
+++ app/src/main/java/com/careitapp/care_it/OcrDetectorProcessor.java
@@ -0,0 +1,32 @@
+package com.careitapp.care_it;
+
+import android.util.SparseArray;
+
+import com.careitapp.care_it.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> mGraphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        mGraphicOverlay = ocrGraphicOverlay;
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        mGraphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+            mGraphicOverlay.add(graphic);
+        }
+    }
+
+    @Override
+    public void release() {
+        mGraphicOverlay.clear();
+    }
+}
