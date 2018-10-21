ba87989a871bc38f80cab25d25b9c1d46c935c5e
abbyzhang1027 <aaz10@duke.edu>
2018-10-21 01:28:58 -0400

    recyclerview update

--- app/src/main/java/com/careitapp/care_it/PillBox.java
+++ app/src/main/java/com/careitapp/care_it/PillBox.java
@@ -2,12 +2,20 @@ package com.careitapp.care_it;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class PillBox extends AppCompatActivity {
+
+    private RecyclerView pillRecyclerView;
+    private RecyclerView.Adapter pillRecyclerAdapter;
+    private RecyclerView.LayoutManager pillRecyclerManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pill_box);
+
+
+
     }
 }
