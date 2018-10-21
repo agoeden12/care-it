3d65f9e49c8c54e10dab6c6481474693f5f69df0
abbyzhang1027 <aaz10@duke.edu>
2018-10-21 02:18:52 -0400

    recyclerview update

--- app/src/main/java/com/careitapp/care_it/PillBox.java
+++ app/src/main/java/com/careitapp/care_it/PillBox.java
@@ -2,7 +2,11 @@ package com.careitapp.care_it;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PillBox extends AppCompatActivity {
 
@@ -15,6 +19,18 @@ public class PillBox extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pill_box);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        pillRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        pillRecyclerManager = new LinearLayoutManager(this);
+        pillRecyclerView.setLayoutManager(pillRecyclerManager);
+
+        // specify an adapter (see also next example)
+        String[] testerDataset = {"this","is","a","test"};
+        pillRecyclerAdapter = new MyAdapter(testerDataset);
+        pillRecyclerView.setAdapter(pillRecyclerAdapter);
 
 
     }
