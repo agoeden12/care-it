649ce9f17b50db1834587f5db246edc9348998c1
smwhitt <smw81@duke.edu>
2018-10-21 06:15:50 -0400

    sms

--- app/src/main/java/com/careitapp/care_it/Notifications.java
+++ app/src/main/java/com/careitapp/care_it/Notifications.java
@@ -1,15 +1,26 @@
 package com.careitapp.care_it;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Notifications extends AppCompatActivity {
+    //private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+    long startTime;
+    String phoneNo = "6783580275";
+    String message = "Your loved one hasn't taken their medication yet! Would you like to call?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,7 @@ public class Notifications extends AppCompatActivity {
         //setSupportActionBar(toolbar);
 
         Spinner dropdown = findViewById(R.id.spinner);
-        String[] items = new String[]{"5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00","11:30"};
+        String[] items = new String[]{"5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
@@ -32,6 +43,58 @@ public class Notifications extends AppCompatActivity {
         String[] items3 = new String[]{"5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00"};
         ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items3);
         dropdown3.setAdapter(adapter3);
+
+        Button secretButt = (Button) findViewById(R.id.secretBtn);
+        secretButt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startTime = System.currentTimeMillis();
+            }
+        });
+
+        long time = System.currentTimeMillis();
+        Long difference = new Long(time - startTime);
+        Long base = new Long(12000000);
+        int comparison = difference.compareTo(base);
+
+        if (comparison > 0) {
+            sendSMSMessage();
+        }
     }
+
+        protected void sendSMSMessage() {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            /*if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.SEND_SMS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.SEND_SMS)) {
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.SEND_SMS},
+                            MY_PERMISSIONS_REQUEST_SEND_SMS);
+                }
+            }*/
+        }
+
+       /* @Override
+        public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults)
+        {
+            switch (requestCode) {
+                case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                        Toast.makeText(getApplicationContext(), "SMS sent.",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+            }
+        }*/
 
 }
