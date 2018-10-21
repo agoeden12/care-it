46e02342fd9b6218859b7b5fbffb210bf337d963
Andrew Goeden <andrewgoeden@gmail.com>
2018-10-21 05:08:12 -0400

    merge

--- app/src/main/java/com/careitapp/care_it/HomeFragment.java
+++ app/src/main/java/com/careitapp/care_it/HomeFragment.java
@@ -1,14 +1,12 @@
 package com.careitapp.care_it;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,7 +23,6 @@ import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class HomeFragment extends Fragment {
 
